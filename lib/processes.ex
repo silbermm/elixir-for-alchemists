909aa02d20ff6ec5b7d6b420ceafb65e7afdf613
Matt Silbernagel <silbermm@gmail.com>
2019-09-16 20:42:02 -0400

    add more process code

--- lib/processes.ex
+++ lib/processes.ex
@@ -1,9 +1,39 @@
 defmodule Processes do
-  def listen do
-    receive do
-      {:ok, "hello"} -> IO.puts("Alchemy")
-    end
+  @moduledoc """
+  Simply show how processes work
+  """
 
-    listen
+  @doc """
+  Hello process that takes an `{:ok, "hello", send_to}` and sends a response to the send_to process
+  """
+  def hello do
+    receive do
+      {:ok, "hello", send_to} ->
+        IO.inspect("IN HELLO PROCESS")
+        send(send_to, "Alchemy")
+    end
+  end
+
+  @doc """
+  Process that prints out a message
+  """
+  def message do
+    receive do
+      msg -> IO.inspect(msg, label: "FROM MESSAGE PROCESS")
+    end
+  end
+
+  @doc """
+  Spawn two processes and send messages
+  """
+  def listen do
+    hello_pid = spawn(Processes, :hello, [])
+    message_pid = spawn(Processes, :message, [])
+
+    IO.inspect("FROM LISTEN FUNCTION")
+
+    send(hello_pid, {:ok, "hello", message_pid})
+
+    IO.inspect("DONE CALLING SEND")
   end
 end
